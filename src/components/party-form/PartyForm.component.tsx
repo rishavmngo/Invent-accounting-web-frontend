073aaf9bdc756eb2b1048311a41ed61731438715
rishavmngo <rishavinmngo@gmail.com>
2025-04-23 14:57:52 +0530

    party form ui completed

--- src/components/party-form/PartyForm.component.tsx
+++ src/components/party-form/PartyForm.component.tsx
@@ -8,14 +8,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "../ui/form";
 import { Input } from "../ui/input";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
@@ -26,15 +24,29 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
+import { Checkbox } from "../ui/checkbox";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+type tabOptions = "addresses" | "gstDetails";
 
 const PartyForm = () => {
   const [open, setOpen] = useState(true);
+  const [currentTab, setCurrentTab] = useState<tabOptions>("gstDetails");
   const form = useForm<PartyFormT>({
     resolver: zodResolver(PartyFormSchema),
     defaultValues: {
       name: "",
       // as_of_date: new Date(),
       receivable: true,
+      gst_type: "unregisterd-consumer",
+      state: "kerala",
     },
   });
 
@@ -44,10 +56,13 @@ const PartyForm = () => {
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
+      <DialogContent className="">
         <DialogTitle>Add new party</DialogTitle>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-4 h-[600px] overflow-y-scroll p-1"
+          >
             <FormField
               control={form.control}
               name="name"
@@ -96,65 +111,192 @@ const PartyForm = () => {
               )}
             />
 
-            <FormField
-              control={form.control}
-              name="opening_balance"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-[var(--invent-gray)] text-lg">
-                    Opening Balance
-                  </FormLabel>
-                  <FormControl>
-                    <Input placeholder="" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="as_of_date"
-              render={({ field }) => (
-                <FormItem className="flex flex-col">
-                  <FormLabel className="text-[var(--invent-gray)] text-lg">
-                    As of Date
-                  </FormLabel>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
+            <section className="flex flex-col gap-2">
+              <div className="grid gap-2">
+                <FormField
+                  control={form.control}
+                  name="opening_balance"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-[var(--invent-gray)] text-lg">
+                        Opening Balance
+                      </FormLabel>
                       <FormControl>
-                        <Button
-                          variant={"outline"}
-                          className={cn(
-                            "w-[240px] pl-3 text-left font-normal",
-                            !field.value && "text-muted-foreground",
-                          )}
-                        >
-                          {field.value ? (
-                            format(field.value, "PPP")
-                          ) : (
-                            <span>Pick a date</span>
-                          )}
-                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                        </Button>
+                        <Input placeholder="" {...field} />
                       </FormControl>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-auto p-0" align="start">
-                      <Calendar
-                        mode="single"
-                        selected={field.value}
-                        onSelect={field.onChange}
-                        disabled={(date) =>
-                          date > new Date() || date < new Date("1900-01-01")
-                        }
-                        initialFocus
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="as_of_date"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col">
+                      <FormLabel className="text-[var(--invent-gray)] text-lg">
+                        As of Date
+                      </FormLabel>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "w-[240px] pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground",
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                          className="w-auto p-0"
+                          align="start"
+                        >
+                          <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            disabled={(date) =>
+                              date > new Date() || date < new Date("1900-01-01")
+                            }
+                            initialFocus
+                          />
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                control={form.control}
+                name="receivable"
+                render={({ field }) => (
+                  <FormItem className="flex">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
                       />
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+                    </FormControl>
+                    <div className="space-y-1 leading-none">
+                      <FormLabel>Receivable</FormLabel>
+                    </div>
+                  </FormItem>
+                )}
+              />
+            </section>
+
+            <Tabs
+              className="w-full "
+              value={currentTab}
+              onValueChange={(value) => setCurrentTab(value as tabOptions)}
+            >
+              <TabsList className="w-full">
+                <TabsTrigger value="addresses">Addresses</TabsTrigger>
+                <TabsTrigger value="gstDetails">GST Details</TabsTrigger>
+              </TabsList>
+              <TabsContent value="addresses">
+                <FormField
+                  control={form.control}
+                  name="billing_address"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-[var(--invent-gray)] text-lg">
+                        Billing Address
+                      </FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="email_address"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-[var(--invent-gray)] text-lg">
+                        Email address
+                      </FormLabel>
+                      <FormControl>
+                        <Input placeholder="" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </TabsContent>
+              <TabsContent className="flex flex-col gap-4" value="gstDetails">
+                <FormField
+                  control={form.control}
+                  name="gst_type"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>GST Type</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select a verified email to display" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="unregisterd-consumer">
+                            Unregisterd/Consumer
+                          </SelectItem>
+                          <SelectItem value="registerd-regular">
+                            Registerd-Regular
+                          </SelectItem>
+                          <SelectItem value="registerd-composite">
+                            Registerd-Composite
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="state"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>State</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="state" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="haryana">Haryana</SelectItem>
+                          <SelectItem value="bihar">Bihar</SelectItem>
+                          <SelectItem value="kerala">Kerala</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </TabsContent>
+            </Tabs>
           </form>
         </Form>
       </DialogContent>
