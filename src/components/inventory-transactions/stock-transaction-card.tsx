40f357ded80a8e4a18f3a1aa885f45e704e84c8b
rishavmngo <rishavinmngo@gmail.com>
2025-07-10 11:53:09 +0530

    added ui for  modify options for item's stocks

--- src/components/inventory-transactions/stock-transaction-card.tsx
+++ src/components/inventory-transactions/stock-transaction-card.tsx
@@ -1,8 +1,26 @@
+"use client";
 import { formatCurrency, formatDate } from "@/lib/utils";
-import React from "react";
+import React, { useState } from "react";
 import { FaLongArrowAltDown, FaLongArrowAltUp } from "react-icons/fa";
-import { FaBoxOpen, FaPlus } from "react-icons/fa6";
-
+import { FaBoxOpen } from "react-icons/fa6";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { CiMenuKebab } from "react-icons/ci";
+import { MdDelete, MdEdit } from "react-icons/md";
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "../ui/alert-dialog";
+import { Button } from "../ui/button";
 type StockTransactionCardProps = {
   quantity: number;
   date: string;
@@ -16,20 +34,54 @@ const StockTransactionCard = ({
   quantity,
   pricePerUnit,
 }: StockTransactionCardProps) => {
+  const [isDeleteDialogOpen, toggleDeleteDialogOpen] = useState(false);
   return (
-    <li className="group flex flex-col gap-4 px-5 py-2 text-sm rounded-md bg-[#F5F5F5] cursor-pointer rainbow-border-wrapper ">
+    <li className="group flex flex-col gap-4 px-5 py-2 text-sm rounded-md bg-[#F5F5F5] ">
       <div className="flex justify-between">
-        <h2 className="font-medium text-xl text-[#4E4E4E]">{type}</h2>
-        {type == "reduce" ? (
-          <span className=" bg-[#f6cccc] p-2 text-[#7d5d5d] flex gap-2 items-center rounded-full">
-            <FaBoxOpen className="text-xl " />
-            <FaLongArrowAltDown className="" />
-          </span>
-        ) : (
-          <span className=" bg-[#daf6cc] p-2 text-[#5d7d5d] flex gap-2 items-center rounded-full">
-            <FaBoxOpen className="text-xl " />
-            <FaLongArrowAltUp className="" />
-          </span>
+        <div className="flex items-center gap-4">
+          <h2 className="font-medium text-xl text-[#4E4E4E]">{type}</h2>
+          {type == "reduce" ? (
+            <span className=" bg-[#f6cccc] p-2 text-[#7d5d5d] flex gap-2 items-center rounded-full">
+              <FaBoxOpen className="text-xl " />
+              <FaLongArrowAltDown className="" />
+            </span>
+          ) : (
+            <span className=" bg-[#daf6cc] p-2 text-[#5d7d5d] flex gap-2 items-center rounded-full">
+              <FaBoxOpen className="text-xl " />
+              <FaLongArrowAltUp className="" />
+            </span>
+          )}
+        </div>
+        {type != "opening_stock" && (
+          <div className="flex gap-4 items-center">
+            <DropdownMenu modal={false}>
+              <DropdownMenuTrigger asChild>
+                <CiMenuKebab />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-full" align="start">
+                <DropdownMenuLabel>Modify stock</DropdownMenuLabel>
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <MdEdit /> Edit
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="text-red-600"
+                    onClick={(event) => {
+                      // event.preventDefault();
+                      toggleDeleteDialogOpen(true);
+                    }}
+                    // onClick={() => {
+                    //   setTimeout(() => {
+                    //     toggleDeleteDialogOpen(true);
+                    //   }, 0);
+                    // }}
+                  >
+                    <MdDelete className="text-red-800" /> Delete
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         )}
       </div>
       <ul className="flex gap-20 ">
@@ -49,6 +101,37 @@ const StockTransactionCard = ({
           </div>
         </li>
       </ul>
+
+      <AlertDialog
+        open={isDeleteDialogOpen}
+        onOpenChange={toggleDeleteDialogOpen}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Are you sure?
+              {/* <span className="font-bold">{data?.name}</span>&quot; ? */}
+            </AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter className="">
+            <div className="flex gap-6">
+              <Button
+                className=""
+                onClick={() => toggleDeleteDialogOpen(false)}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="outline"
+                className="border-red-400 text-red-400 hover:bg-red-400 hover:text-white"
+                onClick={() => {}}
+              >
+                Confirm
+              </Button>
+            </div>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </li>
   );
 };
